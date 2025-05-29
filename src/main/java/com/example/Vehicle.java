4c2f1849dc043c110cd7e9cd57d8eec74e54bc77
olegkopylov <nahallien@gmail.com>
2025-05-29 16:37:23 +0300

    4 задача конструкторы и наследование

---
+++ src/main/java/com/example/Vehicle.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Vehicle {
+    String typeTransport;
+
+    public Vehicle(String typeTransport) {
+        this.typeTransport = typeTransport;
+        System.out.println(typeTransport);
+    }
+}
+
+
+
