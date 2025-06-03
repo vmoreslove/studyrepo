8c1cc35aed0a2acfb429976670c7955c11ad8b14
vmoreslove <nahallien@gmail.com>
2025-06-03 11:24:25 +0300

    Update Person.java

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -6,18 +6,15 @@ class Person {
     String name;
     int age;
 
-    // Конструктор
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Геттер для возраста
     public int getAge() {
         return age;
     }
 
-    // Переопределение метода toString для удобного вывода
     @Override
     public String toString() {
         return name + " (" + age + ")";
