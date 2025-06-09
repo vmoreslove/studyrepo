98157419cf84ee8c4ce37990cc87738855efbb92
olegkopylov <nahallien@gmail.com>
2025-06-09 23:31:00 +0300

    1. Инкапсуляция: Класс BankAccount

--- src/main/java/com/example/BankAccount.java
+++ src/main/java/com/example/BankAccount.java
@@ -1,37 +1,36 @@
 package com.example;
 
 public class BankAccount {
-    int balance;
-    int accountNumber;
-    public BankAccount(int balance, int accountNumber) {
+    private double balance;
+    private int accountNumber;
+    public BankAccount(double balance, int accountNumber) {
         this.balance = balance;
         this.accountNumber = accountNumber;
     }
-    public void deposit(int amount) {
-        System.out.println(accountNumber);
+
+    public double getBalance() {
+        return balance;
     }
 
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 
+    public void deposit(double amount) {
+       balance = balance + amount;
+    }
 
     @Override
     public String toString() {
-        return "BankAccount{" +
-                "balance=" + balance +
-                ", accountNumber=" + accountNumber +
-                '}';
+        return "BankAccount{" + "balance=" + balance + ", accountNumber=" + accountNumber + '}';
     }
 
     public void withdraw(int amount){
-        System.out.println(balance <= balance);
+        balance = balance - amount;
+        if(balance < 0){
+            balance = 0;
+        }
     //не должно позволяться снимать больше, чем есть
-
     }
-
-
-
-
-
-
-
 
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,15 +3,18 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         BankAccount bAccount = new BankAccount(10,15);
-        bAccount.withdraw(16);
-        System.out.println(bAccount);
+        bAccount.deposit(5);
+        bAccount.withdraw(17);
+       
+
+        System.out.println(bAccount.getBalance());
 
     }
 
 }
 /*
 1. Инкапсуляция: Класс BankAccount
-Описание: Создайте класс BankAccount с полями balance, accountNumber. Реализуйте методы:
+Описание:Создайте класс BankAccount с полями balance, accountNumber. Реализуйте методы:
 	•	deposit(double amount)
 	•	withdraw(double amount)
 	•	getBalance()
