e044f761cd03ddd2a55578cd466e988584ce3028
olegkopylov <nahallien@gmail.com>
2025-06-11 12:12:06 +0300

    разбор условия

--- src/main/java/com/example/BankAccount.java
+++ src/main/java/com/example/BankAccount.java
@@ -7,15 +7,12 @@ public class BankAccount {
         this.balance = balance;
         this.accountNumber = accountNumber;
     }
-
     public double getBalance() {
         return balance;
     }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
     public void deposit(double amount) {
        balance = balance + amount;
     }
